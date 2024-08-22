ff7c812257edddf436ab153670530cf39eef94f1
DarkStorm <150134437+DarkStormIX@users.noreply.github.com>
2024-08-22 21:21:02 +0800

    Create button to change grid size

--- script.js
+++ script.js
@@ -1,15 +1,60 @@
 const container = document.querySelector(".container");
+// default value
+var gridSize = 16;
+var containerWidth = 400;
 
-var size = 16
-for(let i = 0; i< size * size; i++){
-    const square = document.createElement("div");
-    square.className = "square";
-    container.appendChild(square);
+function createGrid(gridSize){
+    for(let i = 0; i< gridSize * gridSize; i++){
+        // create grid as canvas
+        const square = document.createElement("div");
+        const width = (containerWidth / gridSize) - 2;
+
+        // setup square for proper display
+        square.style.width = `${width}px`;
+        square.style.height = `${width}px`;
+        square.className = "square";
+        container.appendChild(square);
+    }
+
+    // enable ability to colour
+    const square = document.querySelectorAll(".square");
+    colourSquare(square);
 }
 
-const square = document.querySelectorAll(".square");
-square.forEach((currentValue) => {
-    currentValue.addEventListener("mouseenter", () => {
-        currentValue.style.backgroundColor = "violet";
+createGrid(gridSize);
+
+function removeGrid(){
+    const square = document.querySelectorAll(".square");
+    square.forEach(function(currentValue){
+        currentValue.remove();
     })
-});
+}
+
+function createNewGrid(){
+    const square = document.querySelectorAll(".square");
+    newSize = Number(window.prompt("Enter grid size: "));
+    if(newSize > 100){
+        return window.prompt("Size cannot exceed 100.")
+    }
+    else{
+        removeGrid(square);
+        createGrid(newSize);    
+    }
+}
+
+function colourSquare(square){
+    square.forEach((currentValue) => {
+        currentValue.addEventListener("mouseenter", () => {
+            currentValue.style.backgroundColor = "violet";
+        })
+    });
+}
+
+const pageBody = document.querySelector("body");
+
+const sizeButton = document.createElement("button");
+sizeButton.className = "size-button";
+sizeButton.textContent = "Change Grid Size";
+
+pageBody.appendChild(sizeButton);
+sizeButton.addEventListener("click",createNewGrid);
